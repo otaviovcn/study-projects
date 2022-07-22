d54b083cb2a604f44876bfea6a4bace831b1ab1e
Otavio Vinicius <viinident@gmail.com>
2022-07-21 21:47:41 -0300

    Cria arquivo data.js com as cores

---
+++ front-end-projects/input-colors/src/data.js
@@ -0,0 +1,32 @@
+const colors = [
+    {
+      color: "red",
+      value: "#f00", //hexadecimal de cor
+    },
+    {
+      color: "green",
+      value: "#0f0",
+    },
+    {
+      color: "blue",
+      value: "#00f",
+    },
+    {
+      color: "cyan",
+      value: "#0ff",
+    },
+    {
+      color: "magenta",
+      value: "#f0f",
+    },
+    {
+      color: "yellow",
+      value: "#ff0",
+    },
+    {
+      color: "black",
+      value: "#000",
+    },
+  ];
+  
+  export default colors;
