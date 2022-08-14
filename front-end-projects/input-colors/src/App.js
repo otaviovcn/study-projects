3643977a4792ade9c0c3b3bf86600410175ed203
Otavio Vinicius <viinident@gmail.com>
2022-08-14 14:12:56 -0300

    Commit initial

--- front-end-projects/input-colors/src/App.js
+++ front-end-projects/input-colors/src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import colors from './data';
 import './App.css';
 
-function App() {
-  return (
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props)
     
-  );
+    this.state = {
+      selectedColor: ''
+    }
+  }
+  
+  
+  setFilterValue(event) {
+    this.setState({ selectedColor: event.target.value })
+  }
+
+  
+  
+  render() {
+
+    const colorFiltered = colors.filter((color) => color.color.includes(this.state.selectedColor))
+
+    const list = colorFiltered.map(({ color, value }) => {
+      return <li key={value} style={{ background: value }}>{color}</li>
+    })
+    return (
+      <main>
+        <input type="text" placeholder="Digite uma cor" onChange={(event) => this.setFilterValue(event)}></input>
+
+        <ul>
+          {list}
+        </ul>
+      </main>
+    );
+  }
 }
 
 export default App;
